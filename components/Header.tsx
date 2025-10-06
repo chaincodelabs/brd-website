5a10857f26221946fe7e141fe16de58eccbbe70d
sbddesign <stephen@d.elor.me>
2025-10-06 11:56:43 +0200

    mobile menu toggle

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { useState } from 'react'
 import Pill from "./Pill"
 
 const headerMenu = [
@@ -20,6 +23,12 @@ const headerMenu = [
 ]
 
 export default function Header(){
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen)
+    }
+
     return(
         <header className="flex flex-col font-sans gap-2">
             <div className="flex flex-row justify-between w-full items-center p-6">
@@ -28,14 +37,28 @@ export default function Header(){
                     <Pill text="Midtown NYC" uri="https://chaincode.com" />
                     
                 </div>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                    <path d="M3.75 10.5A.75.75 0 0 1 3 9.75V3.752a.75.75 0 0 1 .75-.75h6a.75.75 0 0 1 .75.75V9.75a.75.75 0 0 1-.75.75h-6Zm10.5 0a.75.75 0 0 1-.75-.75V3.752a.75.75 0 0 1 .75-.75h5.998a.75.75 0 0 1 .75.75V9.75a.75.75 0 0 1-.75.75H14.25ZM3.75 21a.75.75 0 0 1-.75-.75v-6a.75.75 0 0 1 .75-.75h6a.75.75 0 0 1 .75.75v6a.75.75 0 0 1-.75.75h-6Zm10.5 0a.75.75 0 0 1-.75-.75v-6a.75.75 0 0 1 .75-.75h5.998a.75.75 0 0 1 .75.75v6a.75.75 0 0 1-.75.75H14.25Z"/>
-                </svg>
+                <button 
+                    onClick={toggleMenu}
+                    className="lg:hidden text-white hover:text-gray-300 transition-colors"
+                    aria-label="Toggle menu"
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                        <path d="M3.75 10.5A.75.75 0 0 1 3 9.75V3.752a.75.75 0 0 1 .75-.75h6a.75.75 0 0 1 .75.75V9.75a.75.75 0 0 1-.75.75h-6Zm10.5 0a.75.75 0 0 1-.75-.75V3.752a.75.75 0 0 1 .75-.75h5.998a.75.75 0 0 1 .75.75V9.75a.75.75 0 0 1-.75.75H14.25ZM3.75 21a.75.75 0 0 1-.75-.75v-6a.75.75 0 0 1 .75-.75h6a.75.75 0 0 1 .75.75v6a.75.75 0 0 1-.75.75h-6Zm10.5 0a.75.75 0 0 1-.75-.75v-6a.75.75 0 0 1 .75-.75h5.998a.75.75 0 0 1 .75.75v6a.75.75 0 0 1-.75.75H14.25Z"/>
+                    </svg>
+                </button>
             </div>
-            <ul className="bg-black/20">
+            <ul className={`bg-black/20 transition-all duration-300 ease-in-out ${
+                isMenuOpen ? 'max-h-96 opacity-100' : 'max-h-0 opacity-0 overflow-hidden'
+            } lg:max-h-none lg:opacity-100 lg:overflow-visible`}>
             {headerMenu.map((item, index) => (
                 <li key={index}>
-                    <a href={item.uri} className="!text-white !no-underline py-4 px-4 block hover:bg-black/20 active:bg-black/20">{item.name}</a>
+                    <a 
+                        href={item.uri} 
+                        className="!text-white !no-underline py-4 px-4 block hover:bg-black/20 active:bg-black/20"
+                        onClick={() => setIsMenuOpen(false)}
+                    >
+                        {item.name}
+                    </a>
                 </li>
             ))}
             </ul>
