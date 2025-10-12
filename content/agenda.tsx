9be6c78d5b946b28710d457b9032ec59eab8a6b5
sbddesign <stephen@d.elor.me>
2025-10-12 11:53:16 -0400

    date fix and agenda tbd

--- content/agenda.tsx
+++ content/agenda.tsx
@@ -4,20 +4,20 @@ export const Agenda:AgendaEntry[] = [
     {
         time: "9:00 EDT",
         type: "single",
-        content: [{title: "Welcome and Opening Remarks", speaker: "All the guests are welcomed."}]
+        content: [{title: "Welcome", speaker: "TBD"}]
     },
     {
         time: "10:00 - 10:15 CET EDT",
         type: "single",
-        content: [{title: "Opening remarks", speaker: "Clara Shikhelman and Sergi Delgado"}]
+        content: [{title: "Opening remarks", speaker: "TBD"}]
     },
     {
         time: "10:15 - 11:20 EDT",
         type: "multi",
         content: [
-            {title: "De-anonymization in Bitcoin", speaker: "Sarah Meiklejohn"},
-            {title: "Privacy and Scaling with zk-SNARKs and Client-Side Validation", speaker: "Jonas Nick"},
-            {title: "Combining Cryptographic Protocols", speaker: "Josie Baker"}
+            {title: "TBD", speaker: "TBD"},
+            {title: "TBD", speaker: "TBD"},
+            {title: "TBD", speaker: "TBD"}
         ]
     },
     {
@@ -29,7 +29,7 @@ export const Agenda:AgendaEntry[] = [
     {
         time: "11:40 - 12:30 EDT",
         type: "single",
-        content: [{title: "Bitcoin Development Discussion", speaker: "Group Session"}]
+        content: [{title: "TBD", speaker: "TBD"}]
     },
     {
         time: "11:20 - 11:40 EDT",
@@ -41,20 +41,20 @@ export const Agenda:AgendaEntry[] = [
         time: "14:15 - 15:40 EDT",
         type: "multi",
         content: [
-            {title: "Strategic Latency Reduction in Blockchain Peer-to-Peer Networks", speaker: "Guilia Fanti"},
-            {title: "Lighting Pathfinding Privacy and Reliability", speaker: "Matt Corallo"},
-            {title: "Modeling Aspects of the Bitcoin Universe", speaker: "Hannes Hartenstein and Matthias Grundmann"}
+            {title: "TBD", speaker: "TBD"},
+            {title: "TBD", speaker: "TBD"},
+            {title: "TBD", speaker: "TBD"}
         ]
     },
     {
         time: "15:40 - 16:40 EDT",
         type: "single",
-        content: [{title: "MuSig2 and FROST", speaker: "Chelsea Komlo and Jonas Nick (Bitcoin research prize winners)"}]
+        content: [{title: "TBD", speaker: "TBD"}]
     },
     {
         time: "16:40 - 16:50 EDT",
         type: "single",
-        content: [{title: "Closing remarks", speaker: "Clara Shikhelman and Sergi Delgado"}]
+        content: [{title: "Talk Title", speaker: "TBD"}]
     },
     {
         time: "11:20 - 11:40 CET EDT",
