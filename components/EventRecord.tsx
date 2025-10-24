3e0ef927368e285b19dffb86cb31540295f3bf80
sbddesign <stephen@d.elor.me>
2025-10-24 12:57:55 -0400

    hover trophy for prize

--- components/EventRecord.tsx
+++ components/EventRecord.tsx
@@ -16,17 +16,20 @@ export default function EventRecord({
 }: EventRecordProps) {
   return (
     <div className={`mb-6 ${className}`}>
-      {prize && (
-          <div className="inline-block mb-2 bg-yellow-200 bg-gradient-to-r from-orange-300  via-yellow-200 to-orange-300  text-yellow-950 px-3 py-1 rounded-full text-sm font-medium font-sans">
-            <span className='mr-3'>🏆</span>
-            {prize}
-          </div>
-      )}
+      
       <div className="flex flex-row gap-3 items-center">
         <h3 className="text-xl text-white">
           <span className="mr-3">{title}</span>
           {videoUrl && (
             <VideoLink href={videoUrl} />
+          )}
+          {prize && (
+            <div className="inline-flex items-center group relative ml-3">
+              <TrophyIcon className="h-5 w-5 text-yellow-500 transition-colors" />
+              <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-2 py-1 text-base bg-yellow-200 bg-gradient-to-r from-orange-300  via-yellow-200 to-orange-300  text-yellow-950 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none whitespace-nowrap z-10 font-sans">
+                {prize}
+              </span>
+            </div>
           )}
         </h3>
       </div>
