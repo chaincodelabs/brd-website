b8a83d310bdd4f1aa0b07914c5c44aee46dd0093
Sergi Delgado Segura <sergi.delgado.s@gmail.com>
2025-10-30 12:17:34 +0100

    Add 2025 agenda

--- content/data/agenda.tsx
+++ content/data/agenda.tsx
@@ -1,62 +1,67 @@
 import type { AgendaEntry } from "@/components/AgendaItem";
 
 export const Agenda:AgendaEntry[] = [
-    // {
-    //     time: "9:00 EDT",
-    //     type: "single",
-    //     content: [{title: "Welcome", speaker: "Doors open"}]
-    // },
-    // {
-    //     time: "10:00 - 10:15 EDT",
-    //     type: "single",
-    //     content: [{title: "Opening remarks", speaker: "Clara Shikhelman & Sergi Delgado"}]
-    // },
-    // {
-    //     time: "10:15 - 11:20 EDT",
-    //     type: "multi",
-    //     content: [
-    //         {title: "Title of Talk", speaker: "Alice Speaker"},
-    //         {title: "Title of Talk", speaker: "Bob Speaker"},
-    //         {title: "Title of Talk", speaker: "Charlotte Speaker"}
-    //     ]
-    // },
-    // {
-    //     time: "11:20 - 11:40 EDT",
-    //     type: "single",
-    //     content: [{title: "Coffee", speaker: "Break time"}]
-    // },
-    // {
-    //     time: "11:40 - 12:30 EDT",
-    //     type: "single",
-    //     content: [{title: "Title of Talk", speaker: "David Speaker"}]
-    // },
-    // {
-    //     time: "11:20 - 11:40 EDT",
-    //     type: "single",
-    //     content: [{title: "Lunch", speaker: "Break time"}]
-    // },
-    // {
-    //     time: "14:15 - 15:40 EDT",
-    //     type: "multi",
-    //     content: [
-    //         {title: "Title of Talk", speaker: "Emily Speaker"},
-    //         {title: "Title of Talk", speaker: "Frank Speaker"},
-    //         {title: "Title of Talk", speaker: "Gina Speaker"}
-    //     ]
-    // },
-    // {
-    //     time: "15:40 - 16:40 EDT",
-    //     type: "single",
-    //     content: [{title: "Title of Talk", speaker: "Harry Speaker"}]
-    // },
-    // {
-    //     time: "16:40 - 16:50 EDT",
-    //     type: "single",
-    //     content: [{title: "Closing Remarks", speaker: "Clara Shikhelman & Sergi Delgado"}]
-    // },
-    // {
-    //     time: "11:20 - 11:40 CET EDT",
-    //     type: "single",
-    //     content: [{title: "Happy Hour", speaker: "Have fun"}]
-    // }
+    {
+        time: "9:00 EDT",
+        type: "single",
+        content: [{title: "Doors open", speaker: ""}]
+    },
+    {
+        time: "10:00 - 10:15 EDT",
+        type: "single",
+        content: [{title: "Opening remarks", speaker: "Clara Shikhelman & Sergi Delgado"}]
+    },
+    {
+        time: "10:15 - 11:20 EDT",
+        type: "multi",
+        content: [
+            {title: "Failing Productively", speaker: "Aviv Zohar"},
+            {title: "Monitoring Bitcoin’s P2P network", speaker: "b10c"},
+            {title: "Featured Report-Out from BRW25", speaker: "That famous Alice"}
+        ]
+    },
+    {
+        time: "11:20 - 11:40 EDT",
+        type: "single",
+        content: [{title: "Coffee Break", speaker: ""}]
+    },
+    {
+        time: "11:40 - 12:30 EDT",
+        type: "single",
+        content: [{title: "Group Session: Collaborative exploration", speaker: ""}]
+    },
+    {
+        time: "12:30 - 14:15 EDT",
+        type: "single",
+        content: [{title: "Lunch Break", speaker: ""}]
+    },
+    {
+        time: "14:15 - 15:20 EDT",
+        type: "multi",
+        content: [
+            {title: "Bitcoin Scaling", speaker: "Neha Narula"},
+            {title: "Ark and Research Challenges", speaker: "Veronika Knuett"},
+            {title: "Featured Report-Outs from BRW25", speaker: "That really well known Bob"}
+        ]
+    },
+    {
+        time: "15:20 - 15:40 EDT",
+        type: "single",
+        content: [{title: "Coffee Break", speaker: ""}]
+    },
+    {
+        time: "15:40 - 16:40 EDT",
+        type: "single",
+        content: [{title: "Bitcoin research prize ceremony and talk", speaker: "Secret Speaker(s)"}]
+    },
+    {
+        time: "16:40 - 16:50 EDT",
+        type: "single",
+        content: [{title: "Closing Remarks", speaker: "Clara Shikhelman & Sergi Delgado"}]
+    },
+    {
+        time: "16:50 - 18:00 CET EDT",
+        type: "single",
+        content: [{title: "Happy Hour", speaker: ""}]
+    }
 ]
